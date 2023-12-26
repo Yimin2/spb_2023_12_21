c042019e331da55ee79f13cc80c469c4c645bdeb
Yimin2 <gownfla14@naver.com>
2023-12-26 10:21:57 +0900

    list view, article view 추가

--- src/main/java/com/ll/spb_2023_12_19_2/Spb202312192Application.java
+++ src/main/java/com/ll/spb_2023_12_19_2/Spb202312192Application.java
@@ -2,7 +2,9 @@ package com.ll.spb_2023_12_19_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Spb202312192Application {
 
